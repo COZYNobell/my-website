deef315e6e8e88ef082ad65b5f393a233a785bd2
COZYNobell <tlstprp33@gmail.com>
2025-06-16 17:29:50 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,32 +8,98 @@ terraform {
 }
 
 provider "aws" {
-  alias  = "seoul"
-  region = "ap-northeast-2"
+  region = var.aws_region
 }
 
-provider "aws" {
-  alias  = "tokyo"
-  region = "ap-northeast-1"
+# -------------------------
+# GitHub Actions용 IAM Role (OIDC 연동)
+# -------------------------
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
 }
 
-module "seoul" {
-  source    = "./envs/seoul"
-  providers = { aws = aws.seoul }
-
-  db_name         = var.db_name_seoul
-  db_user         = var.db_user
-  db_password     = var.db_password
-  route53_zone_id = var.route53_zone_id
+data "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsAdminRole"
 }
 
-module "tokyo" {
-  source    = "./envs/tokyo"
-  providers = { aws = aws.tokyo }
+# -------------------------
+# VPC 생성 (terraform-aws-modules 사용)
+# -------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.2"
 
-  db_name         = var.db_name_tokyo
-  db_user         = var.db_user
-  db_password     = var.db_password
-  route53_zone_id = var.route53_zone_id
-  primary_rds_arn = module.seoul.rds_arn
+  name = "weather-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
+
+# -------------------------
+# EKS 클러스터 생성
+# -------------------------
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.medium"]
+      key_name       = var.key_pair_name
+    }
+  }
+}
+
+# -------------------------
+# Outputs
+# -------------------------
+output "github_actions_role_arn" {
+  description = "IAM Role ARN for GitHub Actions"
+  value       = data.aws_iam_role.github_actions_role.arn
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_oidc_provider" {
+  value = module.eks.oidc_provider_arn
 }

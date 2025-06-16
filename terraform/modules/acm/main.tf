799a865e805c6e95688fbdb0f8281cd0b5080230
COZYNobell <tlstprp33@gmail.com>
2025-06-17 07:20:08 +0900

    Create main.tf

---
+++ terraform/modules/acm/main.tf
@@ -0,0 +1,24 @@
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "weather-app-cert"
+  }
+}
+
+resource "aws_route53_record" "validation" {
+  name    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_type
+  zone_id = var.route53_zone_id
+  records = [tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_value]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [aws_route53_record.validation.fqdn]
+}

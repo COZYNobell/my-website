45aeb7d02ae7dbd1ca61b9a1df910e319111f4b7
COZYNobell <tlstprp33@gmail.com>
2025-06-17 08:57:02 +0900

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,21 @@
+# ~/terraform/modules/network/variables.tf
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for public subnets"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for private subnets"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "AZs corresponding to the subnets"
+}

230cdaba84dd16f8addf6564bc3665ee2146b11e
COZYNobell <tlstprp33@gmail.com>
2025-06-16 11:36:56 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,27 +11,3 @@ variable "cluster_name" {
   type        = string
   default     = "my-weather-app-cluster"
 }
-
-variable "github_repository" {
-  description = "The GitHub repository (owner/repo) for IAM role trust."
-  type        = string
-  default     = "COZYNobell/my-website"
-}
-
-variable "db_name" {
-  description = "The name of the RDS database."
-  type        = string
-  default     = "master_db"
-}
-
-variable "db_username" {
-  description = "The master username for the RDS database."
-  type        = string
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "The master password for the RDS database."
-  type        = string
-  sensitive   = true
-}

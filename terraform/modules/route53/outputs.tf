3bbd8c1e2084129900eccf6ea00f4d9306a5f509
COZYNobell <tlstprp33@gmail.com>
2025-06-17 07:27:51 +0900

    Update outputs.tf

--- terraform/modules/route53/outputs.tf
+++ terraform/modules/route53/outputs.tf
@@ -1,3 +1,9 @@
 output "route53_record_name" {
-  value = aws_route53_record.weather_app.name
+  description = "The DNS record name"
+  value       = aws_route53_record.weather_app.name
+}
+
+output "route53_record_fqdn" {
+  description = "The full domain name"
+  value       = "${aws_route53_record.weather_app.name}.${var.route53_zone_id}"
 }

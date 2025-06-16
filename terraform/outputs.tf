2c21d972b0945d407f52cd1021631e00d592a20f
COZYNobell <tlstprp33@gmail.com>
2025-06-16 17:31:03 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,19 @@
+output "github_actions_role_arn" {
+  description = "GitHub Actions가 사용하는 IAM Role의 ARN"
+  value       = data.aws_iam_role.github_actions_role.arn
+}
+
+output "db_name" {
+  description = "RDS에서 사용할 DB 이름 (기본값)"
+  value       = "weather"
+}
+
 output "seoul_rds_endpoint" {
-  value = module.seoul.rds_endpoint
+  description = "서울 RDS 인스턴스의 엔드포인트"
+  value       = module.seoul.rds_endpoint
 }
 
 output "tokyo_rds_endpoint" {
-  value = module.tokyo.rds_endpoint
-}
-
-output "global_alb_dns" {
-  value = module.seoul.alb_dns_name
+  description = "도쿄 RDS 복제본의 엔드포인트"
+  value       = module.tokyo.rds_endpoint
 }

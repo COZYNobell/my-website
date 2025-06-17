1e84f1f27ed03d03b551755ed12988b41a44c812
COZYNobell <tlstprp33@gmail.com>
2025-06-17 10:22:23 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,106 @@
 # terraform/main.tf
 
-# 서울과 도쿄, 두 개의 리전에 대한 AWS 공급자를 정의합니다.
-provider "aws" {
-  alias  = "seoul"
-  region = "ap-northeast-2"
+# 이 파일에서는 별도의 provider 블록을 선언하지 않습니다.
+# 같은 폴더 내의 다른 .tf 파일에 있는 provider 설정을 공유하여 사용합니다.
+# (만약 없다면, 이전에 드린 최종본의 provider 블록을 추가해야 합니다.)
+
+
+# --- 1. 네트워크 (VPC) ---
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.2"
+
+  name = "my-eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
 
-provider "aws" {
-  alias  = "tokyo"
-  region = "ap-northeast-1"
+# --- 2. EKS 클러스터 ---
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # EKS API 서버 엔드포인트를 외부에서 접속할 수 있도록 활성화
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t3.medium"]
+      key_name       = "Seoul-ec22-key"
+    }
+  }
 }
 
-# 서울 리전의 인프라를 정의하는 모듈을 호출합니다.
-module "seoul" {
-  source    = "./envs/seoul"
-  providers = { aws = aws.seoul } # 이 모듈은 서울 리전 프로바이더를 사용합니다.
-
-  # 루트의 variables.tf를 통해 전달받은 변수들을 서울 모듈에 전달합니다.
-  db_name         = var.db_name_seoul
-  db_user         = var.db_user
-  db_password     = var.db_password
-  route53_zone_id = var.route53_zone_id
+# --- 3. RDS 데이터베이스 ---
+resource "aws_db_subnet_group" "app_rds_subnet_group" {
+  name       = "app-rds-subnet-group-for-eks"
+  subnet_ids = module.vpc.private_subnets
 }
 
-# 도쿄 리전의 인프라를 정의하는 모듈을 호출합니다.
-module "tokyo" {
-  source    = "./envs/tokyo"
-  providers = { aws = aws.tokyo } # 이 모듈은 도쿄 리전 프로바이더를 사용합니다.
+resource "aws_security_group" "rds_sg" {
+  name        = "app-rds-access-sg"
+  description = "Allow EKS nodes to access RDS"
+  vpc_id      = module.vpc.vpc_id
 
-  # 루트의 variables.tf를 통해 전달받은 변수들을 도쿄 모듈에 전달합니다.
-  db_name         = var.db_name_tokyo
-  db_user         = var.db_user
-  db_password     = var.db_password
-  route53_zone_id = var.route53_zone_id
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [module.eks.node_security_group_id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-  # ✨ 도쿄 RDS Replica가 서울 RDS를 복제하기 위해, 서울 모듈의 출력값(ARN)을 전달합니다.
-  primary_rds_arn = module.seoul.rds_arn
+resource "aws_db_instance" "app_db" {
+  identifier           = "weather-app-db"
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.app_rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot  = true
+}
+
+# --- 4. 출력 값 ---
+output "rds_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.app_db.endpoint
+}
+output "rds_db_name" {
+  description = "The name of the database"
+  value       = aws_db_instance.app_db.db_name
 }

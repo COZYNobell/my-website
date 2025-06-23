c02d9e138d7ecb188859a764a206dd39b0c3176f
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:03:49 +0900

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,16 +1,7 @@
-# ~/terraform/modules/alb/outputs.tf
-
-output "alb_arn" {
-  description = "The ARN of the ALB"
-  value       = aws_lb.this.arn
-}
-
 output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.this.dns_name
+  value = aws_lb.this.dns_name
 }
 
 output "target_group_arn" {
-  description = "The ARN of the target group"
-  value       = aws_lb_target_group.this.arn
+  value = aws_lb_target_group.this.arn
 }

6ef866e1d3debb1378c68a6ec63e49445588bb15
COZYNobell <tlstprp33@gmail.com>
2025-06-23 13:20:34 +0900

    Update outputs.tf

--- terraform/envs/seoul/outputs.tf
+++ terraform/envs/seoul/outputs.tf
@@ -1,26 +1,14 @@
-# RDS ARN (도쿄 리전에서 참조)
-output "rds_arn" {
-  description = "ARN of the primary RDS instance (for replica)"
-  value       = module.rds.arn
+output "eks_cluster_name" {
+  description = "Name of the EKS Cluster"
+  value       = aws_eks_cluster.alpha_eks.name
 }
 
-# RDS Endpoint (예: Route53 등록용 등)
-output "rds_endpoint" {
-  description = "Primary RDS endpoint"
-  value       = module.rds.endpoint
+output "eks_cluster_endpoint" {
+  description = "EKS Cluster endpoint"
+  value       = aws_eks_cluster.alpha_eks.endpoint
 }
 
-# VPC ID (필요 시 외부 참조 가능)
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
-# Public Subnet IDs (예: ALB 등에서 사용 가능)
-output "public_subnets" {
-  value = module.network.public_subnets
-}
-
-# Private Subnet IDs
-output "private_subnets" {
-  value = module.network.private_subnets
+output "eks_cluster_certificate" {
+  description = "EKS Cluster Certificate Authority"
+  value       = aws_eks_cluster.alpha_eks.certificate_authority[0].data
 }

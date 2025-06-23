d9bfb9e2ab9beb340667d3ce37fe762c4cc137f9
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:05:01 +0900

    Update outputs.tf

--- modules/route53/outputs.tf
+++ modules/route53/outputs.tf
@@ -1,13 +1,3 @@
-# ~/terraform/modules/route53/outputs.tf
-
-output "record_name" {
-  description = "The DNS record name"
-  value       = aws_route53_record.this.name
-}
-
-output "fqdn" {
-  description = "Fully qualified domain name of the record"
-  # Route 53 zone data source is needed to get the zone name for FQDN
-  # For now, we output the record part only.
-  value       = aws_route53_record.this.name
+output "cname_record" {
+  value = aws_route53_record.cname.fqdn
 }

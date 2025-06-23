e644d6942b8a778b446443e184ae542f4a0ce6c1
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:33:00 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "cozynobell"
+
+    workspaces {
+      name = "alpha-multiregion"
+    }
+  }
+}
+
+# --- Provider Definitions for Multiple Regions ---
+provider "aws" {
+  alias  = "seoul"
+  region = "ap-northeast-2"
+}
+
+provider "aws" {
+  alias  = "tokyo"
+  region = "ap-northeast-1"
+}
+
+# --- Seoul Region Module ---
 module "seoul" {
   source = "./envs/seoul"
+
   providers = {
     aws = aws.seoul
   }
@@ -11,8 +41,10 @@ module "seoul" {
   route53_zone_id  = var.route53_zone_id
 }
 
+# --- Tokyo Region Module ---
 module "tokyo" {
   source = "./envs/tokyo"
+
   providers = {
     aws = aws.tokyo
   }

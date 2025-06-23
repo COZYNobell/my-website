93846fc9b5da5771549a420909bcd64345111fa1
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:35:44 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  cloud {
-    organization = "cozynobell"
-
-    workspaces {
-      name = "alpha-multiregion"
-    }
-  }
-}
-
-# --- Provider Definitions for Multiple Regions ---
 provider "aws" {
   alias  = "seoul"
   region = "ap-northeast-2"
@@ -26,13 +8,9 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# --- Seoul Region Module ---
 module "seoul" {
-  source = "./envs/seoul"
-
-  providers = {
-    aws = aws.seoul
-  }
+  source = "../envs/seoul"  # 또는 ./envs/seoul (상대 경로 확인 필요)
+  providers = { aws = aws.seoul }
 
   vpc_cidr_block   = "10.1.0.0/16"
   db_name          = var.db_name_seoul
@@ -41,13 +19,9 @@ module "seoul" {
   route53_zone_id  = var.route53_zone_id
 }
 
-# --- Tokyo Region Module ---
 module "tokyo" {
-  source = "./envs/tokyo"
-
-  providers = {
-    aws = aws.tokyo
-  }
+  source = "../envs/tokyo"
+  providers = { aws = aws.tokyo }
 
   vpc_cidr_block   = "10.2.0.0/16"
   db_name          = var.db_name_tokyo

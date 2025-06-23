3352a468843c9bad1c31f992f22494a9434a2905
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:04:41 +0900

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,16 +1,11 @@
-# ~/terraform/modules/route53/variables.tf
-
 variable "zone_id" {
-  type        = string
-  description = "The Route53 Hosted Zone ID"
+  type = string
 }
 
 variable "record_name" {
-  type        = string
-  description = "The name of the DNS record to create (e.g., db.myapp.com)"
+  type = string
 }
 
 variable "cname_value" {
-  type        = string
-  description = "The value of the CNAME record (e.g., RDS endpoint)"
+  type = string
 }

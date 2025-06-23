4d75e457c7d729abda0e75d2f045baf9bd400083
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:02:26 +0900

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,26 +1,29 @@
-# terraform/modules/network/outputs.tf
-
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnets" {
-  description = "List of IDs for the public subnets"
-  value       = [for s in aws_subnet.public : s.id]
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
-output "private_subnet_group" {
-  description = "The name of the DB Subnet Group"
-  value       = aws_db_subnet_group.this.name
-}
-
-output "rds_sg_id" {
-  description = "The ID of the security group for RDS"
-  value       = aws_security_group.rds.id
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
 }
 
 output "alb_sg_id" {
-  description = "The ID of the security group for the ALB"
+  description = "Security group ID for ALB"
   value       = aws_security_group.alb.id
+}
+
+output "rds_sg_id" {
+  description = "Security group ID for RDS"
+  value       = aws_security_group.rds.id
+}
+
+output "private_subnet_group" {
+  description = "RDS용 Subnet Group 이름"
+  value       = aws_db_subnet_group.private.name
 }

77670f4a6ca6fd946d0bf85bcfb1b6198c0dce33
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:06:56 +0900

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,21 +1,15 @@
-# terraform/modules/network/variables.tf
-
 variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
+  type = string
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
+  type = list(string)
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
+  type = list(string)
 }
 
 variable "availability_zones" {
-  description = "List of Availability Zones for the subnets"
-  type        = list(string)
+  type = list(string)
 }

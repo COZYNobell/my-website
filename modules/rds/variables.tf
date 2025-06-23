7181b01dd22f6e2679ee555df87700ec0a1ba490
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:00:26 +0900

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,54 +1,51 @@
-# ~/terraform/modules/rds/variables.tf
-
 variable "identifier" {
-  description = "Unique identifier for the RDS instance"
+  description = "RDS instance identifier"
   type        = string
 }
 
 variable "instance_class" {
-  description = "RDS instance type"
+  description = "RDS instance class"
   type        = string
 }
 
 variable "storage" {
-  description = "Allocated storage in GB"
+  description = "Allocated storage for RDS"
   type        = number
 }
 
 variable "db_name" {
-  description = "Database name"
+  description = "Initial database name"
   type        = string
 }
 
 variable "db_user" {
-  description = "Database master username"
+  description = "Master username"
   type        = string
 }
 
 variable "db_password" {
-  description = "Database master password"
+  description = "Master password"
   type        = string
   sensitive   = true
 }
 
 variable "db_subnet_group" {
-  description = "Subnet group name for RDS"
+  description = "Subnet group for RDS"
   type        = string
 }
 
 variable "security_group_ids" {
-  description = "List of security group IDs for the RDS instance"
+  description = "List of security group IDs"
   type        = list(string)
 }
 
-variable "replicate_source_arn" {
-  description = "ARN of source DB to replicate from (for CRRR)"
-  type        = string
-  default     = null
+variable "multi_az" {
+  description = "Whether to enable Multi-AZ"
+  type        = bool
 }
 
-variable "multi_az" {
-  type        = bool
-  description = "Whether to enable Multi-AZ deployment for RDS"
-  default     = false
+variable "replicate_source_db" {
+  description = "ARN of the source DB instance to replicate (if Read Replica)"
+  type        = string
+  default     = null
 }

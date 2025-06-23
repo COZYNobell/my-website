56738d69cec1d28ecc772276aa3d3503082b50dc
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:01:45 +0900

    Update outputs.tf

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,16 +1,19 @@
-# ~/terraform/modules/rds/outputs.tf
-
-output "endpoint" {
-  description = "RDS endpoint to connect to"
-  value       = aws_db_instance.this.endpoint
-}
-
+# 출력: RDS 인스턴스 ARN (서울 or 도쿄)
 output "arn" {
-  description = "ARN of the RDS instance"
-  value       = aws_db_instance.this.arn
+  description = "ARN of the RDS instance (primary or replica)"
+  value = (
+    length(aws_db_instance.main) > 0 ?
+    aws_db_instance.main[0].arn :
+    aws_db_instance.replica[0].arn
+  )
 }
 
-output "identifier" {
-  description = "RDS instance identifier"
-  value       = aws_db_instance.this.id
+# 출력: RDS 인스턴스 엔드포인트
+output "endpoint" {
+  description = "Endpoint of the RDS instance"
+  value = (
+    length(aws_db_instance.main) > 0 ?
+    aws_db_instance.main[0].endpoint :
+    aws_db_instance.replica[0].endpoint
+  )
 }

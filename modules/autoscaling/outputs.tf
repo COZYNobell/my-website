4c60cf730a81238f3338ac1a06ed038e5ba9bcfb
COZYNobell <tlstprp33@gmail.com>
2025-06-23 10:04:27 +0900

    Update outputs.tf

--- modules/autoscaling/outputs.tf
+++ modules/autoscaling/outputs.tf
@@ -1,11 +1,3 @@
-# ~/terraform/modules/autoscaling/outputs.tf
-
-output "launch_template_id" {
-  description = "The ID of the launch template"
-  value       = aws_launch_template.this.id
-}
-
 output "asg_name" {
-  description = "The name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.this.name
+  value = aws_autoscaling_group.asg.name
 }
